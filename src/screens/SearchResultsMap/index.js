7f32c155b97d24446a886714fb2956157d30a0f8
Ahmed Tarek <a7medelnashar@hotmail.com>
2021-09-24 08:08:08 +0200

    added maps - places pins functionality

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FlatList, useWindowDimensions, View } from "react-native";
 
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
@@ -9,10 +9,42 @@ import styles from "./styles";
 import PostCarouselItem from "../../components/PostCarouselItem";
 const SearchResultsMapScreen = () => {
   const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+
+  const flatList = useRef();
+  const map = useRef();
+
+  const viewabilityConfig = useRef({ itemVisiblePercentThreshold: 70 });
+
+  const onViewChanged = useRef(({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      const selectedPlace = viewableItems[0].item;
+      setSelectedPlaceId(selectedPlace.id);
+    }
+  });
+
+  useEffect(() => {
+    if (!selectedPlaceId || !flatList) {
+      return;
+    }
+    const index = feed.findIndex((place) => place.id === selectedPlaceId);
+    flatList.current.scrollToIndex({ index });
+
+    const selectedPlace = feed[index];
+    const region = {
+      latitude: selectedPlace.coordinate.latitude,
+      longitude: selectedPlace.coordinate.longitude,
+      latitudeDelta: 0.2,
+      longitudeDelta: 0.2,
+    };
+    map.current.animateToRegion(region);
+  }, [selectedPlaceId]);
+
   const width = useWindowDimensions().width;
+
   return (
     <View style={{ height: "100%", width: "100%" }}>
       <MapView
+        ref={map}
         style={{ height: "100%", width: "100%" }}
         provider={PROVIDER_GOOGLE}
         initialRegion={{
@@ -33,6 +65,7 @@ const SearchResultsMapScreen = () => {
       </MapView>
       <View style={styles.postCarouselContainer}>
         <FlatList
+          ref={flatList}
           data={feed}
           renderItem={({ item }) => <PostCarouselItem post={item} />}
           horizontal
@@ -40,6 +73,8 @@ const SearchResultsMapScreen = () => {
           snapToInterval={width - 6}
           snapToAlignment={"center"}
           decelerationRate={"fast"}
+          viewabilityConfig={viewabilityConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
